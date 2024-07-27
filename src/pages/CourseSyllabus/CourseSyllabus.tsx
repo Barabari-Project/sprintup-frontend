f5a6c904e89e4dbe8b74f3cd772d4853441c14ed
drumil32 <akheniad@gmail.com>
2024-07-27 15:17:01 +0530

    event added

--- src/pages/CourseSyllabus/CourseSyllabus.tsx
+++ src/pages/CourseSyllabus/CourseSyllabus.tsx
@@ -6,7 +6,7 @@ import restEndPoints from "../../data/restEndPoints.json";
 import axiosInstance, { eventAxiosInstance } from "../../utils/axiosInstance";
 import { EventType } from "../../types/types";
 import { FaGraduationCap } from "react-icons/fa6";
-import { MdFlightClass, MdOutlineLock } from "react-icons/md";
+import { MdFlightClass, MdOutlineCheck, MdOutlineLock } from "react-icons/md";
 import { SiInternetarchive, SiGoogleclassroom } from "react-icons/si";
 import { GrProjects } from "react-icons/gr";
 import { GiFaceToFace } from "react-icons/gi";
@@ -21,6 +21,7 @@ type SubTopic = {
   name: string;
   description: string;
   isLocked: boolean;
+  link?: string;
 };
 
 type Topic = {
@@ -152,6 +153,12 @@ const LessonItem: React.FC<{ topic: Topic }> = ({ topic }) => {
     setIsExpanded((prev) => !prev);
   };
 
+  const triggerEvent = () => {
+    eventAxiosInstance.post(`/${restEndPoints.eventAuth}`, {
+      type: EventType.LOCK_CLICK,
+    });
+  }
+
   return (
     <div className={styles.lessonContainer}>
       <h3
@@ -175,21 +182,39 @@ const LessonItem: React.FC<{ topic: Topic }> = ({ topic }) => {
             <p className={styles.lessonDescription}>{topic.description}</p>
             <div className={styles.topicsContainer}>
               {topic.subtopics.map((subTopic) => (
-                <div className={styles.topic} key={nanoid()}>
-                  <div className={styles.topicIcon}>
-                    <MdOutlineLock />
+                subTopic.link ? (
+                  <a
+                    href={subTopic.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.topicLink}
+                  >
+                    <div className={styles.topic} key={nanoid()}>
+                      <div className={styles.topicIcon}>
+                        {subTopic.isLocked ? <MdOutlineLock /> : <MdOutlineCheck />}
+                      </div>
+                      <div className={styles.topicContent}>
+                        <h5>{subTopic.name}</h5>
+                        <p>{subTopic.description}</p>
+                      </div>
+                    </div>
+                  </a>
+                ) :
+                  <div className={styles.topic} key={nanoid()} onClick={() => triggerEvent()}>
+                    <div className={styles.topicIcon}>
+                      {subTopic.isLocked ? <MdOutlineLock /> : <MdOutlineCheck />}
+                    </div>
+                    <div className={styles.topicContent}>
+                      <h5>{subTopic.name}</h5>
+                      <p>{subTopic.description}</p>
+                    </div>
                   </div>
-                  <div className={styles.topicContent}>
-                    <h5>{subTopic.name}</h5>
-                    <p>{subTopic.description}</p>
-                  </div>
-                </div>
               ))}
             </div>
-          </motion.div>
+          </motion.div >
         )}
-      </AnimatePresence>
-    </div>
+      </AnimatePresence >
+    </div >
   );
 };
 
